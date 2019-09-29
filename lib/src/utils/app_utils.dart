11535cfaa6030608f9fc96c8ccce8e6789a65b6e
tungpham <phamthanhtung1995@gmail.com>
2019-09-29 09:44:00 +0700

    nothing

--- lib/src/utils/app_utils.dart
+++ lib/src/utils/app_utils.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/foundation.dart';
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
-class AppUtils {
-  static bool isMobileOS() {
-    return defaultTargetPlatform == TargetPlatform.android ||
-        defaultTargetPlatform == TargetPlatform.iOS ||
-        defaultTargetPlatform == TargetPlatform.fuchsia;
-  }
+bool isMobileOS() {
+  return defaultTargetPlatform == TargetPlatform.android ||
+      defaultTargetPlatform == TargetPlatform.iOS ||
+      defaultTargetPlatform == TargetPlatform.fuchsia;
+}
 
-  static bool isDesktopOS() {
-    return defaultTargetPlatform != TargetPlatform.android &&
-        defaultTargetPlatform != TargetPlatform.iOS &&
-        defaultTargetPlatform == TargetPlatform.fuchsia;
-  }
+bool isDesktopOS() {
+  return defaultTargetPlatform != TargetPlatform.android &&
+      defaultTargetPlatform != TargetPlatform.iOS &&
+      defaultTargetPlatform == TargetPlatform.fuchsia;
+}
 
-  static void mobileLaunchUrl(url) async {
-    try {
-      if (await url_launcher.canLaunch(url)) {
-        await url_launcher.launch(url);
-      } else {
-        throw 'Could not launch $url';
-      }
-    } catch (e) {
-      print(e.toString());
+void mobileLaunchUrl(url) async {
+  try {
+    if (await url_launcher.canLaunch(url)) {
+      await url_launcher.launch(url);
+    } else {
+      throw 'Could not launch $url';
     }
+  } catch (e) {
+    print(e.toString());
   }
 }
