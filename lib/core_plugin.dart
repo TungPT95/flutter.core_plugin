01527a786058a93d748732619a134ac4dce66d55
tungpham <phamthanhtung1995@gmail.com>
2019-10-01 23:52:36 +0700

    add screen_utils.dart

--- lib/core_plugin.dart
+++ lib/core_plugin.dart
@@ -1,9 +1,10 @@
 library core_plugin;
 
 export 'src/base_state.dart';
-export 'src/utils/text_utils.dart';
-export 'src/utils/app_utils.dart';
-export 'src/utils/validate_utils.dart';
-export 'src/layouts/responsive/utils/layout_utils.dart';
 export 'src/layouts/responsive/responsive_layout.dart';
 export 'src/layouts/responsive/responsive_widget.dart';
+export 'src/layouts/responsive/utils/layout_utils.dart';
+export 'src/utils/app_utils.dart';
+export 'src/utils/screen_utils.dart';
+export 'src/utils/text_utils.dart';
+export 'src/utils/validate_utils.dart';
