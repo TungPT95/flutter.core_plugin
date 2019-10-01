17176c756a91a0c335ff925bb898b0d25288a3d8
tungpham <phamthanhtung1995@gmail.com>
2019-10-01 23:51:43 +0700

    add screen_utils.dart

--- lib/src/base_state.dart
+++ lib/src/base_state.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
 
+///width of Iphone 5 device
+const double designWidth = 375.0;
+
+///height of Iphone 5 device
+const double designHeight = 667.0;
+
 abstract class BaseState<T extends StatefulWidget> extends State<T> {
   ThemeData get theme => Theme.of(context);
 
   Size get screenSize => MediaQuery.of(context).size;
 
-  ///width of Iphone 5 device
-  double _designWidth = 375.0;
-
-  ///height of Iphone 5 device
-  double _designHeight = 667.0;
 
   double scaleWidth(double width) {
-    return width * screenSize.width / _designWidth;
+    return width * screenSize.width / designWidth;
   }
 
   double scaleHeight(double height) {
-    return height * screenSize.height / _designHeight;
+    return height * screenSize.height / designHeight;
   }
 
-  double screenWidthRatio() => screenSize.width / _designWidth;
+  double screenWidthRatio() => screenSize.width / designWidth;
 
-  double screenHeightRatio() => screenSize.height / _designHeight;
+  double screenHeightRatio() => screenSize.height / designHeight;
 
   double screenWidthFraction(double percent) {
     return screenSize.width * percent / 100;
---
+++ lib/src/utils/screen_utils.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+import '../base_state.dart';
+
+class ScreenUtils {
+  static Size screenSize(context) => MediaQuery.of(context).size;
+
+  static double scaleWidth(BuildContext context, double width) {
+    return width * screenSize(context).width / designWidth;
+  }
+
+  static double scaleHeight(BuildContext context, double height) {
+    return height * screenSize(context).height / designHeight;
+  }
+
+  static double screenWidthRatio(BuildContext context) =>
+      screenSize(context).width / designWidth;
+
+  static double screenHeightRatio(BuildContext context) =>
+      screenSize(context).height / designHeight;
+
+  static double screenWidthFraction(BuildContext context, double percent) {
+    return screenSize(context).width * percent / 100;
+  }
+
+  static double screenHeightFraction(BuildContext context, double percent) {
+    return screenSize(context).height * percent / 100;
+  }
+}
