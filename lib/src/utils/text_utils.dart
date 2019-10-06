a1a6f6474b082fb867124d2514bb6c7b6483683d
tungpham <phamthanhtung1995@gmail.com>
2019-10-06 22:54:25 +0700

    add percentFormat, milliSecondToString in text_utils.dart

--- lib/src/utils/text_utils.dart
+++ lib/src/utils/text_utils.dart
@@ -1,4 +1,5 @@
 import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' show DateFormat;
 
 String currencyFormat(double amount, String local, String pattern) =>
     NumberFormat.currency(locale: local, customPattern: pattern).format(amount);
@@ -10,3 +11,9 @@ String formatVND(double amount) => currencyFormat(amount, 'vi', '#,###VND');
 ///format to [amount]đ
 ///vd: 1000 => 1.000đ
 String formatDONG(double amount) => currencyFormat(amount, 'vi', '#,###đ');
+
+String percentFormat(double percent) => '$percent%';
+
+String milliSecondToString(int milliseconds, {String format = 'HH:mm:ss'}) =>
+    DateFormat(format)
+        .format(DateTime.fromMillisecondsSinceEpoch(milliseconds));
