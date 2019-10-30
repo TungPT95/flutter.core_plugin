1b443bb1c3e682d3eacd4b212efb899febd685d7
tungpham <phamthanhtung1995@gmail.com>
2019-10-30 23:54:18 +0700

    use static method network.dart

--- lib/src/network/network.dart
+++ lib/src/network/network.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class Network {
-  Dio dio(String baseUrl, {int connectTimeout = 60 * 1000}) =>
+  static Dio dio(String baseUrl, {int connectTimeout = 60 * 1000}) =>
       Dio(BaseOptions(baseUrl: baseUrl, connectTimeout: connectTimeout))
         ..interceptors.addAll([
           PrettyDioLogger(requestBody: true, responseHeader: true),
