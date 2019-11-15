6bc0da515d363fc23c6b58cddb271a0a4e6f3b21
tungpham <phamthanhtung1995@gmail.com>
2019-11-15 19:59:19 +0700

    export some libs

--- lib/core_plugin.dart
+++ lib/core_plugin.dart
@@ -1,7 +1,17 @@
 library core_plugin;
 
+export 'package:bloc/bloc.dart';
+export 'package:bloc_test/bloc_test.dart';
+export 'package:dio_http2_adapter/dio_http2_adapter.dart';
+export 'package:dio_http_cache/dio_http_cache.dart';
+export 'package:equatable/equatable.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:intl/intl.dart';
+export 'package:json_annotation/json_annotation.dart';
 export 'package:logger/logger.dart';
+export 'package:pretty_dio_logger/pretty_dio_logger.dart';
+export 'package:retrofit/retrofit.dart';
+export 'package:shared_preferences/shared_preferences.dart';
 export 'package:url_launcher/url_launcher.dart';
 
 export 'src/base_state.dart';
