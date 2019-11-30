61c9c94bcdaff20d5f3c5bd5c32a4e53b9d85fa9
tungpham <phamthanhtung1995@gmail.com>
2019-11-30 10:42:21 +0700

    update datetime_utils.dart

--- lib/src/utils/datetime_utils.dart
+++ lib/src/utils/datetime_utils.dart
@@ -1,9 +1,10 @@
 import 'package:intl/intl.dart';
 
-const String FORMAT_DEFAULT = 'HH:mm:ss';
-const String FORMAT_1 = 'HH:mm:ss mm/dd/yyyy';
+
 
 class DataTimeUtils {
+  static const String FORMAT_DEFAULT = 'HH:mm:ss';
+  static const String FORMAT_1 = 'HH:mm:ss mm/dd/yyyy';
   static String milliSecondToDateString(int milliseconds,
           {String format = FORMAT_DEFAULT}) =>
       DateFormat(format)
