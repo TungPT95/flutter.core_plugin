8b50030d48821ef4a011b8cd7c3946fee0f7f67a
tungpham <phamthanhtung1995@gmail.com>
2019-11-30 11:00:22 +0700

    update datetime_utils.dart

--- lib/src/utils/datetime_utils.dart
+++ lib/src/utils/datetime_utils.dart
@@ -1,10 +1,21 @@
 import 'package:intl/intl.dart';
 
+const _hour = 'H';
+const _minute = 'm';
+const _second = 's';
+const _day = 'd';
+const _month = 'M';
+const _year = 'y';
+
 class DataTimeUtils {
-  static const String FORMAT_DEFAULT = 'HH:mm:ss';
-  static const String FORMAT_1 = 'HH:mm:ss mm/dd/yyyy';
+  static const String FORMAT_DEFAULT = '${_hour + _hour}:${_minute +
+      _minute}:${_second + _second}';
+  static const String FORMAT_1 = '${_hour + _hour}:${_minute +
+      _minute} ${_day + _day}/${_month + _month}/${_year + _year + _year +
+      _year}';
+
   static String milliSecondToDateString(int milliseconds,
-          {String format = FORMAT_DEFAULT}) =>
+      {String format = FORMAT_DEFAULT}) =>
       DateFormat(format)
-          .format(DateTime.fromMillisecondsSinceEpoch(milliseconds));
+          .format(DateTime.fromMillisecondsSinceEpoch(milliseconds,isUtc: true));
 }
