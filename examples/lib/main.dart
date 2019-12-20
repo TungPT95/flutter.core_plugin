9e71195a2db440f0f96a8ec6ce3ed9ffec6e67c3
tungpham <phamthanhtung1995@gmail.com>
2019-12-20 07:11:59 +0700

    update gradle migrate to androidX

--- examples/lib/main.dart
+++ examples/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'ex_navigator.dart';
+import 'visible_detector/ex_visible_detector.dart';
 
-void main() => runApp(ExNavigatorApp());
+//void main() => runApp(ExNavigatorApp());
+void main() => runApp(ExVisibleDetector.newInstance());
---
+++ examples/lib/visible_detector/ex_visible_detector.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:core_plugin/core_plugin.dart';
+
+class ExVisibleDetector extends StatefulWidget {
+  static Widget newInstance() => MaterialApp(
+        home: ExVisibleDetector._(),
+      );
+
+  ExVisibleDetector._();
+
+  @override
+  _ExVisibleDetectorState createState() => new _ExVisibleDetectorState();
+}
+
+class _ExVisibleDetectorState extends BaseState<ExVisibleDetector> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (_, index) {
+          print('[TUNG] ===> $index');
+          return Text(index.toString());
+        },
+        itemExtent: 70,
+        itemCount: 100,
+      ),
+    );
+  }
+}
