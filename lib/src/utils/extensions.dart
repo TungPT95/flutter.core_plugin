dea355591bf028e05b9219cee0996b5f6af48fab
tungpham <phamthanhtung1995@gmail.com>
2020-06-10 22:08:39 +0700

    add removeVnAccent

--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -12,6 +12,36 @@ extension StringExtension on String {
   bool get isNullOrEmpty => this.isNull || this.isEmpty;
 
   String plus(String s) => this + s;
+
+  String removeVnAccent() {
+    final aRegex = '[áàả,ã,ạ,â,ấ,ầ,ẩ,ẫ,ậ,ă,ắ,ằ,ẳ,ẵ,ặ]';
+    final dRegex = '[đ]';
+    final eRegex = '[é,è,ẻ,ẽ,ẹ,ê,ế,ề,ể,ễ,ệ]';
+    final iRegex = '[í,ì,ỉ,ĩ,ị]';
+    final oRegex = '[ó,ò,ỏ,õ,ọ,ô,ố,ồ,ổ,ỗ,ộ,ơ,ớ,ờ,ở,ỡ,ợ]';
+    final uRegex = '[ú,ù,ủ,ũ,ụ,ư,ứ,ừ,ử,ữ,ự]';
+    final yRegex = '[ý,ỳ,ỷ,ỹ,ỵ]';
+
+    String result = this.isNull
+        ? null
+        : this
+            .replaceAll(RegExp(aRegex), 'a')
+            .replaceAll(RegExp(aRegex.toUpperCase()), 'A')
+            .replaceAll(RegExp(dRegex), 'd')
+            .replaceAll(RegExp(dRegex.toUpperCase()), 'D')
+            .replaceAll(RegExp(eRegex), 'e')
+            .replaceAll(RegExp(eRegex.toUpperCase()), 'E')
+            .replaceAll(RegExp(iRegex), 'i')
+            .replaceAll(RegExp(iRegex.toUpperCase()), 'I')
+            .replaceAll(RegExp(oRegex), 'o')
+            .replaceAll(RegExp(oRegex.toUpperCase()), 'O')
+            .replaceAll(RegExp(uRegex), 'u')
+            .replaceAll(RegExp(uRegex.toUpperCase()), 'U')
+            .replaceAll(RegExp(yRegex), 'y')
+            .replaceAll(RegExp(yRegex.toUpperCase()), 'Y');
+
+    return result;
+  }
 }
 
 extension ListExtension on List {
--- lib/src/utils/text_utils.dart
+++ lib/src/utils/text_utils.dart
@@ -3,44 +3,17 @@ import 'extensions.dart';
 String percentFormat(double percent) => '$percent%';
 
 class TextUtil {
-  static String removeVnAccent(String input) {
-    final aRegex = '[áàả,ã,ạ,â,ấ,ầ,ẩ,ẫ,ậ,ă,ắ,ằ,ẳ,ẵ,ặ]';
-    final dRegex = '[đ]';
-    final eRegex = '[é,è,ẻ,ẽ,ẹ,ê,ế,ề,ể,ễ,ệ]';
-    final iRegex = '[í,ì,ỉ,ĩ,ị]';
-    final oRegex = '[ó,ò,ỏ,õ,ọ,ô,ố,ồ,ổ,ỗ,ộ,ơ,ớ,ờ,ở,ỡ,ợ]';
-    final uRegex = '[ú,ù,ủ,ũ,ụ,ư,ứ,ừ,ử,ữ,ự]';
-    final yRegex = '[ý,ỳ,ỷ,ỹ,ỵ]';
-
-    String result = input.isNull
-        ? ''
-        : input
-            .replaceAll(RegExp(aRegex), 'a')
-            .replaceAll(RegExp(aRegex.toUpperCase()), 'A')
-            .replaceAll(RegExp(dRegex), 'd')
-            .replaceAll(RegExp(dRegex.toUpperCase()), 'D')
-            .replaceAll(RegExp(eRegex), 'e')
-            .replaceAll(RegExp(eRegex.toUpperCase()), 'E')
-            .replaceAll(RegExp(iRegex), 'i')
-            .replaceAll(RegExp(iRegex.toUpperCase()), 'I')
-            .replaceAll(RegExp(oRegex), 'o')
-            .replaceAll(RegExp(oRegex.toUpperCase()), 'O')
-            .replaceAll(RegExp(uRegex), 'u')
-            .replaceAll(RegExp(uRegex.toUpperCase()), 'U')
-            .replaceAll(RegExp(yRegex), 'y')
-            .replaceAll(RegExp(yRegex.toUpperCase()), 'Y');
-
-    return result;
-  }
-
   ///chuỗi [source] có bao gồm [keyword] hay ko
   static bool contains(String source, String keyword) {
-    return removeVnAccent(source.trim().toLowerCase())
-        .contains(removeVnAccent(keyword.trim().toLowerCase()));
+    return source
+        .removeVnAccent()
+        .trim()
+        .toLowerCase()
+        .contains(keyword.removeVnAccent().trim().toLowerCase());
   }
 }
 
 void main() {
-  final abc = TextUtil.removeVnAccent('àá ẮảđẵỉêỹôữỨ');
+  final abc = 'àá ẮảđẵỉêỹôữỨ'.removeVnAccent();
   print('[TUNG] ===> $abc');
 }
