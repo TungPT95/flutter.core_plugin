786b948334ebd9f1f427747e3e0a0e927140875a
tungpham <phamthanhtung1995@gmail.com>
2020-07-15 21:50:31 +0700

    publish dio in network.dart

--- lib/src/network/network.dart
+++ lib/src/network/network.dart
@@ -2,12 +2,10 @@ import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 abstract class Network {
-  Dio _dio;
-
-  Dio get dio => _dio;
+  Dio dio;
 
   Network(String baseUrl, {int connectTimeout}) {
-    _dio = Dio(BaseOptions(
+    dio = Dio(BaseOptions(
         baseUrl: baseUrl, connectTimeout: connectTimeout ?? 60 * 1000))
       ..interceptors.addAll([
         PrettyDioLogger(requestBody: true, responseHeader: true),
