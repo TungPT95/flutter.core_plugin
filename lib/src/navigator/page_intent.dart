cd78d84b1d5de7674ce08d570fbc5c81bde66376
tungpham <phamthanhtung1995@gmail.com>
2020-09-06 22:35:08 +0700

    add rootNavigator

--- lib/src/navigator/page_intent.dart
+++ lib/src/navigator/page_intent.dart
@@ -3,7 +3,10 @@ import 'package:core_plugin/src/navigator/bundle.dart';
 class PageIntent {
   final Type screen;
 
+  ///khi muốn dùng context từ thằng root navigator của app
+  final bool rootNavigator;
+
   Bundle bundle;
 
-  PageIntent({this.screen, this.bundle});
+  PageIntent({this.screen, this.bundle, this.rootNavigator});
 }
