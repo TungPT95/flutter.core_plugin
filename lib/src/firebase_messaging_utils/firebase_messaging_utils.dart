dcddf72ee4a90bcd046fca537f54c1c800e3f524
tungpham <phamthanhtung1995@gmail.com>
2020-12-17 22:24:24 +0700

    add firebase_messaging_utils

---
+++ lib/src/firebase_messaging_utils/firebase_messaging_utils.dart
@@ -0,0 +1,6 @@
+library firebase_messaging_utils;
+
+export 'package:core_plugin/src/firebase_messaging_utils/models/aps/firebase_messaging_aps.dart';
+export 'package:core_plugin/src/firebase_messaging_utils/models/data/base_firebase_messaging_data.dart';
+export 'package:core_plugin/src/firebase_messaging_utils/models/firebase_messaging_response.dart';
+export 'package:core_plugin/src/firebase_messaging_utils/models/notification/firebase_messaging_notification.dart';
