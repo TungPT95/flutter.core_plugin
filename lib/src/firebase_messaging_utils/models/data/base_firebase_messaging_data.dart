a7e21ebead6e26682fd2b8b3c7ae7e688d8b229f
tungpham <phamthanhtung1995@gmail.com>
2020-12-17 23:36:41 +0700

    remove constructor

--- lib/src/firebase_messaging_utils/models/data/base_firebase_messaging_data.dart
+++ lib/src/firebase_messaging_utils/models/data/base_firebase_messaging_data.dart
@@ -3,6 +3,4 @@ import 'package:core_plugin/core_plugin.dart';
 abstract class BaseFirebaseMessagingData {
   @JsonKey(name: 'click_action')
   String clickAction;
-
-  BaseFirebaseMessagingData({this.clickAction});
 }
