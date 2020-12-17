082bde0dc84d7f98c0de8c86282ead327e979f29
tungpham <phamthanhtung1995@gmail.com>
2020-12-17 22:24:35 +0700

    add firebase_messaging_utils

---
+++ lib/src/firebase_messaging_utils/models/firebase_messaging_response.dart
@@ -0,0 +1,19 @@
+import 'package:core_plugin/src/firebase_messaging_utils/models/aps/firebase_messaging_aps.dart';
+import 'package:core_plugin/src/firebase_messaging_utils/models/notification/firebase_messaging_notification.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+abstract class FirebaseMessagingResponse {
+  FirebaseMessagingNotification notification;
+  FirebaseMessagingAps aps;
+
+  @JsonKey(ignore: true)
+  String get title => notification?.title;
+
+  @JsonKey(ignore: true)
+  String get body => notification?.body;
+
+  FirebaseMessagingResponse({
+    this.notification,
+    this.aps,
+  });
+}
