f925f336efa3cc955198e05a686f523dbb6a0129
tungpham <phamthanhtung1995@gmail.com>
2020-12-17 23:19:58 +0700

    add constructor

--- lib/src/firebase_messaging_utils/models/data/base_firebase_messaging_data.dart
+++ lib/src/firebase_messaging_utils/models/data/base_firebase_messaging_data.dart
@@ -3,4 +3,6 @@ import 'package:core_plugin/core_plugin.dart';
 abstract class BaseFirebaseMessagingData {
   @JsonKey(name: 'click_action')
   String clickAction;
+
+  BaseFirebaseMessagingData({this.clickAction});
 }
