65d93765b0053b093beb0b0ae988f549e46c173c
tungpham <phamthanhtung1995@gmail.com>
2020-12-20 01:01:19 +0700

    exports

--- lib/src/firebase_messaging_utils/firebase_messaging_utils.dart
+++ lib/src/firebase_messaging_utils/firebase_messaging_utils.dart
@@ -2,10 +2,15 @@ library firebase_messaging_utils;
 
 import 'dart:convert';
 
+export 'package:core_plugin/src/firebase_messaging_utils/firebase_messaging_interface.dart';
 export 'package:core_plugin/src/firebase_messaging_utils/models/aps/firebase_messaging_aps.dart';
 export 'package:core_plugin/src/firebase_messaging_utils/models/data/base_firebase_messaging_data.dart';
+export 'package:core_plugin/src/firebase_messaging_utils/models/firebase_messaging_pair_object.dart';
 export 'package:core_plugin/src/firebase_messaging_utils/models/firebase_messaging_response.dart';
 export 'package:core_plugin/src/firebase_messaging_utils/models/notification/firebase_messaging_notification.dart';
+export 'package:core_plugin/src/firebase_messaging_utils/models/notification/firebase_messaging_notification.dart';
+export 'package:core_plugin/src/firebase_messaging_utils/module/firebase_messaging_module.dart';
+export 'package:core_plugin/src/firebase_messaging_utils/module/firebase_messaging_module_interface.dart';
 
 ///
 /// class chính để parse trực tiếp json từ Firebase Messaging
