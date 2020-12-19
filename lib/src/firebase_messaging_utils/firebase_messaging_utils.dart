6c9381d2b36d356b5451e1317df18d6528d2180a
tungpham <phamthanhtung1995@gmail.com>
2020-12-19 10:35:14 +0700

    create `FirebaseMessagingUtils`

--- lib/src/firebase_messaging_utils/firebase_messaging_utils.dart
+++ lib/src/firebase_messaging_utils/firebase_messaging_utils.dart
@@ -1,6 +1,14 @@
 library firebase_messaging_utils;
 
+import 'dart:convert';
+
 export 'package:core_plugin/src/firebase_messaging_utils/models/aps/firebase_messaging_aps.dart';
 export 'package:core_plugin/src/firebase_messaging_utils/models/data/base_firebase_messaging_data.dart';
 export 'package:core_plugin/src/firebase_messaging_utils/models/firebase_messaging_response.dart';
 export 'package:core_plugin/src/firebase_messaging_utils/models/notification/firebase_messaging_notification.dart';
+
+class FirebaseMessagingUtils {
+  FirebaseMessagingUtils._();
+
+  static Map<String, dynamic> decodeJson(json) => jsonDecode(jsonEncode(json));
+}
