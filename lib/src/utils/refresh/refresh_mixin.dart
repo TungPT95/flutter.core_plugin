9104fa670d7242b32d03fc112ffca20750404201
tungpham <phamthanhtung1995@gmail.com>
2020-12-29 23:52:02 +0700

    rename

--- lib/src/utils/refresh/refresh_mixin.dart
+++ lib/src/utils/refresh/refresh_mixin.dart
@@ -7,13 +7,13 @@ mixin RefreshMixin<T extends StatefulWidget> on State<T> {
   StreamSubscription _reloadBlocSub;
 
   ///ko cần thiết phải gọi super khi override lại ở sub-class
-  void onReloadWidget(Type widget, Bundle bundle);
+  void onRefreshWidget(Type widget, Bundle bundle);
 
   @override
   void initState() {
     super.initState();
     _reloadBlocSub = context.bloc<RefreshBloc>().listen((state) {
-      onReloadWidget(state.widget, state.bundle);
+      onRefreshWidget(state.widget, state.bundle);
     });
   }
 
