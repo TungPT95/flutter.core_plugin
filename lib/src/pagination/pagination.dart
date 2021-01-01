63a8a74e3ee8d13722f6319ff493c546367b3d88
tungpham <phamthanhtung1995@gmail.com>
2021-01-01 15:18:49 +0700

    refactor

--- lib/src/pagination/pagination.dart
+++ lib/src/pagination/pagination.dart
@@ -1,5 +1,5 @@
 library pagination;
 
-export 'package:core_plugin/src/pagination/src/grid_view/pagination_grid_view.dart';
 export 'package:core_plugin/src/pagination/src/pagination_mixin.dart';
-export 'package:core_plugin/src/pagination/src/refresh/pulldown_refresh_widget.dart';
+export 'package:core_plugin/src/pagination/src/view/grid_view/pagination_grid_view.dart';
+export 'package:core_plugin/src/pagination/src/view/refresh/pulldown_refresh_widget.dart';
--- lib/src/pagination/src/pagination_mixin.dart
+++ lib/src/pagination/src/pagination_mixin.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:core_plugin/core_plugin.dart';
 import 'package:core_plugin/src/pagination/src/pagination_interface.dart';
-import 'package:core_plugin/src/pagination/src/refresh/refresh_interface.dart';
+import 'package:core_plugin/src/pagination/src/view/refresh/refresh_interface.dart';
 import 'package:flutter/foundation.dart';
 
 mixin PaginationMixin<T> implements PaginationInterface<T>, RefreshInterface {
