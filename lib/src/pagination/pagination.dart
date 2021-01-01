70b2c6cbcb0757bdd77d70fb5d41355c8a8fb242
tungpham <phamthanhtung1995@gmail.com>
2021-01-01 15:14:39 +0700

    remove duplicated code

--- lib/src/pagination/pagination.dart
+++ lib/src/pagination/pagination.dart
@@ -3,4 +3,3 @@ library pagination;
 export 'package:core_plugin/src/pagination/src/grid_view/pagination_grid_view.dart';
 export 'package:core_plugin/src/pagination/src/pagination_mixin.dart';
 export 'package:core_plugin/src/pagination/src/refresh/pulldown_refresh_widget.dart';
-export 'package:core_plugin/src/pagination/src/refresh/pulldown_refresh_widget.dart';
