18c1dc71708fa85f2453000f059d2366029db6a0
tungpham <phamthanhtung1995@gmail.com>
2021-01-01 15:34:15 +0700

    export `pagination_list_view`

--- lib/src/pagination/pagination.dart
+++ lib/src/pagination/pagination.dart
@@ -2,4 +2,5 @@ library pagination;
 
 export 'package:core_plugin/src/pagination/src/pagination_mixin.dart';
 export 'package:core_plugin/src/pagination/src/view/grid_view/pagination_grid_view.dart';
+export 'package:core_plugin/src/pagination/src/view/list_view/pagination_list_view.dart';
 export 'package:core_plugin/src/pagination/src/view/refresh/pulldown_refresh_widget.dart';
