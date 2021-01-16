a76ef7df1f74ea08cad02cce070343fae3273b12
tungpham <phamthanhtung1995@gmail.com>
2021-01-16 15:36:28 +0700

    add getter `refreshing`, `loading`

--- lib/src/pagination/src/pagination_interface.dart
+++ lib/src/pagination/src/pagination_interface.dart
@@ -8,4 +8,10 @@ abstract class PaginationInterface<Model> {
   int get page;
 
   int get limit;
+
+  ///[refreshing] = true nếu đang refresh
+  bool get refreshing;
+
+  ///[loading] = true nếu đang loading
+  bool get loading;
 }
